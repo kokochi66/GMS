279e311ad744f64ce5a4d1c1f0c9488874ecfdd6
kokochi66 <kokochikochi@gmail.com>
2021-05-26 17:10:04 +0900

    2021.05.26 minesweeper timer&counter

--- test/test1.js
+++ test/test1.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const expect  = require("chai").expect;
 const request = require('request');
-
+var chai = require('chai'), chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 const url = 'http://localhost:3000'
 
 describe('GET 인덱싱 테스트', () => {
@@ -12,6 +13,11 @@ describe('GET 인덱싱 테스트', () => {
     })
     it('Test :: /login', () => {
         request(`${url}/login`, (err,res,body) => {
+            expect(res.statusCode).to.equal(200)
+        })
+    })
+    it('Test :: /logout', () => {
+        request(`${url}/logout`, (err,res,body) => {
             expect(res.statusCode).to.equal(200)
         })
     })
