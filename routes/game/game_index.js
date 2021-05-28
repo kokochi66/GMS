ce62f3f3f53636580e8fbd6c912a57eea16f2305
kokochi66 <kokochikochi@gmail.com>
2021-05-28 16:39:33 +0900

    2021.05.28 minesweeper

--- routes/game/game_index.js
+++ routes/game/game_index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const mysql = require('mysql')
+const fs = require('fs')
+
+const connKey = JSON.parse(fs.readFileSync('./key/sql.json'))
+const connection = mysql.createConnection({
+    host     : connKey.host,
+    port     : connKey.port,
+    user     : connKey.user,
+    password : connKey.password,
+    database : connKey.database
+});
 
 router.get('/:id', (req,res) => {
+    res.status(200).render('./ejs/game/game.ejs', {'game':req.params.id, 'user':req.cookies.user})
+})
+
+router.post('/:id/:order', (req,res) => {
     res.status(200).render('./ejs/game/game.ejs', {'game':req.params.id, 'user':req.cookies.user})
 })
 
