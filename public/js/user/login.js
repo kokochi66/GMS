b6e60474fda645e555d113ec25a237c9c6a06928
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-07-28 19:21:48 +0900

    StudyFlim으로 리파지토리 이동, 정리

--- public/js/user/login.js
+++
@@ -1,8 +0,0 @@
-$('.login_form').keypress((event) => {
-    if(event.which == 13) {
-    submitPost()
-    }
-})
-$('.login_btn').click(() => {
-    submitPost()
-});
--- routes/user/logout_index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req,res) => {
-    res.status(200).clearCookie('user').redirect(`${req.query.url}`)
-})
-
-module.exports = router
